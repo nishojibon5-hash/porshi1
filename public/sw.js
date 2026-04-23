8a239baf4c6319d17f1d45d9da06c1b6ade8f2ed
salman <nishojibon5@gmail.com>
2026-04-23 20:02:01 +0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = 'porsh-v2';
+const CACHE_NAME = 'porsh-v3'; // ভার্সন ৩ করে দিলাম যাতে ব্রাউজার আপডেট পায়
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json',
+  '/manifest.json', // মেনিফেস্ট ফাইলটি এখানে থাকা জরুরি
   '/sw.js',
-  '/porsh-pwa-icon.png'
+  '/porsh-pwa-icon.png' // আইকন পাথ ঠিক আছে
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -23,6 +24,7 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
